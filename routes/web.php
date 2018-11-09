6c374a13f17762282da9a019226d5bc4e23917b9
juanbarata <juan.barata.si@gmail.com>
2018-11-09 00:12:41 -0300

    Commit da Seção 5 - Aula 14 à 15

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,25 @@ Route::get('/olausuario/repetir/{nome}/{n}', function($nome, $n){
         echo 'Ola '. $nome. '.<br>';
     }
 });
+
+//Seção 5 - Aula 14 - Parâmetros opcionais e restritos
+
+Route::get('/olausuariocomregra/repetir/{nome}/{n}', function($nome, $n){
+    for ($i = 1; $i < $n; $i++){
+        echo 'Ola '. $nome.' '. $i . '.<br>';
+    }
+})->where('n', '[0-9]+');
+
+//Seção 5 - Aula 15 - Agrupamento de rotas
+
+Route::prefix('app')->group(function(){
+    Route::get('/', function(){
+        return 'Página Principal do app.';
+    });
+    Route::get('profile', function(){
+        return 'Página Profile.';
+    });
+    Route::get('about', function(){
+        return 'Página about';
+    });
+});
