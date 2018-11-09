1647b4010740cd731b9c0199eab3ded6d900885c
juanbarata <juan.barata.si@gmail.com>
2018-11-08 23:40:58 -0300

    Commit da Seção 5 - aula 13

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+//Seção 5 - Aula 12
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -19,6 +21,14 @@ Route::get('/olamundo', function () {
     echo '<h1>Olá Mundo!</h1>';
 });
 
-Route::get('/olausuario', function () {
-    echo 'Olá usuário! Seja bem-vindo!';
+//Seção 5 - Aula 13
+
+Route::get('/olausuario/{nome}', function ($nome) {
+    echo 'Olá '.$nome.'! Seja bem-vindo!';
+});
+
+Route::get('/olausuario/repetir/{nome}/{n}', function($nome, $n){
+    for ($i = 0; $i < $n; $i++){
+        echo 'Ola '. $nome. '.<br>';
+    }
 });
